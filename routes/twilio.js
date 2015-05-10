db9990e3e0e71a08c76b704257f7daf7c8d94677
Ali Zahid <ali.zahid@live.com>
2015-05-10 12:40:04 +0400

    Twilio route refactored and beautified

--- routes/twilio.js
+++ routes/twilio.js
@@ -1,74 +1,64 @@
+var SMSAuth = require('../models/sms-auth'),
+	User = require('../models/user');
+
 var twilio = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
-var SMSAuth = require('../models/sms-auth');
-var User = require('../models/user');
 
-exports.getcode = function(req, res) {
-        if (req.body.phone) {
-            var random = Math.floor(Math.random()*(9999-1000+1)+1000);
+exports.getcode = function (req, res) {
+	if (req.body.phone) {
+		var random = Math.floor(Math.random() * (9999 - 1000 + 1) + 1000);
 
-            var smsAuth = new SMSAuth();
+		var smsAuth = new SMSAuth();
 
-            smsAuth.phone = req.body.phone;
-            smsAuth.code = random;
+		smsAuth.phone = req.body.phone;
+		smsAuth.code = random;
 
-            smsAuth.save(function(err, data) {
-                if (err) return res.status(500).send(err);
+		smsAuth.save(function (err, data) {
+			if (err) return res.status(500).send(err);
 
-                twilio.messages.create({
-                    body: 'Use ' + random + ' as your verification code.',
-                    to: req.body.phone,
-                    from: process.env.TWILIO_NUMBER
-                }, function(err, message) {
-                    if (err) {
-                        console.error('twilio', err);
+			twilio.messages.create({
+				body: 'Use ' + random + ' as your verification code.',
+				to: req.body.phone,
+				from: process.env.TWILIO_NUMBER
+			}, function (err, message) {
+				if (err) return res.send(err);
 
-                        return res.send(err);
-                    }
+				res.send({
+					id: data._id
+				});
+			});
+		});
+	}
+};
 
-                    console.log('twilio', message);
+exports.verify = function (req, res) {
+	SMSAuth.findOne({
+		_id: req.params.id,
+		code: req.params.code
+	}, function (err, data) {
+		if (err || !data) return res.status(500).send(err);
 
-                    res.send({
-                        id: data._id
-                    });
-                });
-            });
-        }
-    };
+		data.remove();
 
-exports.verify = function(req, res){
-        SMSAuth.findOne({
-            _id: req.params.id,
-            code: req.params.code
-        }, function(err, data) {
-            if (err || data === null) return res.status(500).send(err);
+		User.findOne({
+			phone: data.phone
+		}, 'token name preferences joined', function (err, user) {
+			if (err) return res.send(404).send(err);
 
-            // delete document
-            data.remove();
-
-            User.findOne({
-                phone: data.phone
-            }, 'token name preferences joined', function(err, user) {
-                if (err) return res.send(404).send(err);
-
-                if (user) {
-                    data.remove();
-
-                    res.send({
-                        token: user.token,
-                        user: {
-                            id: user._id,
-                            name: user.name,
-                            preferences: user.preferences,
-                            joined: user.joined
-                        }
-                    });
-                } else {
-                    res.send({
-                        phone: data.phone
-                    });
-
-                    data.remove();
-                }
-            });
-        });
-    };
+			if (user) {
+				res.send({
+					token: user.token,
+					user: {
+						id: user._id,
+						name: user.name,
+						preferences: user.preferences,
+						joined: user.joined
+					}
+				});
+			} else {
+				res.send({
+					phone: data.phone
+				});
+			}
+		});
+	});
+};
