305b545dd5d7a6b536881f788cda276b30b3a091
Ali Zahid <ali.zahid@live.com>
2015-05-14 17:56:43 +0400

    Refactoring

--- models/plan.js
+++ models/plan.js
@@ -1,55 +1,47 @@
 var mongoose = require('mongoose');
- 
-var schema = new mongoose.Schema(
-{
-    creator:
-    {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    },
-    type: {
-        type: String, // Look into using enum for this in the future. (food, drinks, movies, clubbing, outdoors)
-        index: true
-    },
-    location:
-    {
-        type: [Number],  // [<longitude>, <latitude>]
-        index: '2d'      // create the geospatial index
-    },
-    description: String,
-    sponsored: Boolean,
-    created:
-    {
-        type: Date,
-        default: Date.now
-    },
-    participants:
-    [
-        {
-            time:{
-                type:Date,
-                default: Date.now
-            },
-            user:{
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'User'
-            }
+
+var schema = new mongoose.Schema({
+	creator: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User'
+	},
+	type: {
+		type: String,
+		index: true
+	},
+	location: {
+		type: [Number], // [<longitude>, <latitude>]
+		index: '2d' // create the geospatial index
+	},
+	description: String,
+	sponsored: Boolean,
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	participants: [
+		{
+			time: {
+				type: Date,
+				default: Date.now
+			},
+			user: {
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'User'
+			}
         }
     ],
-    comments:
-    [
-        {
-            user:
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'User'
-            },
-            body: String,
-            time:
-            {
-                type: Date,
-                default: Date.now
-            },
+	comments: [
+		{
+			user: {
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'User'
+			},
+			body: String,
+			time: {
+				type: Date,
+				default: Date.now
+			},
 			auto: {
 				type: Boolean,
 				default: false
@@ -58,27 +50,24 @@ var schema = new mongoose.Schema(
     ]
 });
 
-schema.set('toJSON',
-{
-    transform: function(doc, ret, options)
-    {
-        ret.id = ret._id;
+schema.set('toJSON', {
+	transform: function (doc, ret, options) {
+		ret.id = ret._id;
 
-        if (ret.location) {
-            ret.location = {
+		if (ret.location) {
+			ret.location = {
 				latitude: ret.location[1],
 				longitude: ret.location[0]
-            };
-        }
+			};
+		}
 
-        if (ret.comments) ret.comments = ret.comments.length;
+		if (ret.comments) ret.comments = ret.comments.length;
 
-        if (ret.participants) ret.participants = ret.participants.length + 1;
- 
-        delete ret._id;
-        delete ret.__v;
-    }
- 
+		if (ret.participants) ret.participants = ret.participants.length + 1;
+
+		delete ret._id;
+		delete ret.__v;
+	}
 });
- 
+
 module.exports = mongoose.model('Plan', schema);
